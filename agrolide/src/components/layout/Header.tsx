869f627142e6d6195d84283fd869b862dea04569
Agrolide Admin <admin@agrolide.org>
2026-07-09 01:12:14 +0000

    Add Bibliotheque link to main navigation header

--- agrolide/src/components/layout/Header.tsx
+++ agrolide/src/components/layout/Header.tsx
@@ -9,6 +9,7 @@ const NAV_LINKS = [
   { href: "/qui-sommes-nous", label: "Qui sommes-nous" },
   { href: "/actualites",      label: "Actualités" },
   { href: "/nos-actions",     label: "Nos actions" },
+  { href: "/bibliotheque",    label: "Bibliothèque" },
   { href: "/blog",            label: "Blog" },
 ]
 
