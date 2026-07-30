e343d1b876412eb9b46ec63d9b4c0e4a2ddf324e
Agrolide Admin <admin@agrolide.org>
2026-07-30 14:18:10 +0000

    fix: Type error on implicit any[] for articlesList

--- agrolide/src/app/admin/contenus/articles/page.tsx
+++ agrolide/src/app/admin/contenus/articles/page.tsx
@@ -9,7 +9,7 @@ export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 
 export default async function ArticlesListPage() {
-  let articlesList = []
+  let articlesList: any[] = []
   try {
     articlesList = await db.select().from(articles).orderBy(desc(articles.created_at))
   } catch (e) {
